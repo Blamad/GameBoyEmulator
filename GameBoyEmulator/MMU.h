a122705df937a7a70c9269e4c9c96c3775a06add
Pina92 <pinawoj@gmail.com>
2015-01-13 20:45:16 +0100

    Update MMU.h

--- GameBoyEmulator/MMU.h
+++ GameBoyEmulator/MMU.h
@@ -1,13 +1,29 @@
 #ifndef MMU_H
 #define MMU_H
 
+#include <vector>
+#include <iostream>
+using namespace std;
+
 class MMU
 {
 public:
 	MMU();
+	void init();
 	char rb(int addr);				//Odczytaj bajt z adresu
 	short rw(int addr);				//Odczytaj slowo z adresu
-	void wb(int addr, char byte);	//Zapisz bajt pod adres
-	void ww(int addr, short word);	//Zapisz slowo pod adres
-}
+	void wb(char byte, int addr);	//Zapisz bajt pod adres
+	void ww(short word, int addr);	//Zapisz slowo pod adres
+	vector<char> load(char* filename);	//Zaladowanie Romu 
+
+private:
+	vector<char> _rom;				
+	vector<char> _eram;
+	vector<char> _bios;
+	vector<char> _wram; 
+	vector<char> _zram;
+
+	char _carttype;					//Typ kartidza
+	int _inbios;
+};
 #endif
