118ba57def72537a8f4f40ad249700411487e684
Pina92 <pinawoj@gmail.com>
2015-01-13 22:58:58 +0100

    Update MMU.cpp

--- GameBoyEmulator/MMU.cpp
+++ GameBoyEmulator/MMU.cpp
@@ -56,17 +56,17 @@ char MMU::rb(int addr)
 			case 0x7000: 
 				return _rom[0x4000 + (addr & 0x3FFF)]; 
 				
-			//External RAM (8k)
+			//Zewnetrzny RAM 
 			case 0xA000: 
 			case 0xB000: 
 				return _eram[0x0000 + (addr & 0x1FFF)]; 
 
-			// Graphics: VRAM (8k) 
+			//VRAM 
 			case 0x8000: 
 			case 0x9000: 
-				//return GPU._vram[addr & 0x1FFF]; 
+				//return _vram[addr & 0x1FFF]; 
 			
-			// Working RAM (8k) 
+			// Working RAM
 			case 0xC000: 
 			case 0xD000: 
 				return _wram[addr & 0x1FFF]; 
@@ -80,26 +80,16 @@ char MMU::rb(int addr)
 				switch(addr & 0x0F00) 
 				{ 
 					// Working RAM shadow 
-					case 0x000: 
-					case 0x100: 
-					case 0x200: 
-					case 0x300: 
-					case 0x400: 
-					case 0x500: 
-					case 0x600: 
-					case 0x700: 
-					case 0x800: 
-					case 0x900: 
-					case 0xA00: 
-					case 0xB00: 
-					case 0xC00: 
-					case 0xD00: 
+					case 0x000: case 0x100: case 0x200: 
+					case 0x300: case 0x400: case 0x500: 
+					case 0x600: case 0x700: case 0x800: 
+					case 0x900: case 0xA00: case 0xB00: 
+					case 0xC00: case 0xD00: 
 						return _wram[addr & 0x1FFF]; 
 					
-					// Graphics: object attribute memory 
-					// OAM is 160 bytes, remaining bytes read as 0 
+					// OAM
 					case 0xE00: 
-						//if(addr < 0xFEA0) return GPU._oam[addr & 0xFF]; 
+						//if(addr < 0xFEA0) return _oam[addr & 0xFF]; 
 						//else return 0; 
 					
 					// Zero-page 
@@ -108,8 +98,7 @@ char MMU::rb(int addr)
 							return _zram[addr & 0x7F]; 
 						}
 						else { 
-							// I/O control handling 
-							// Currently unhandled 
+							// I/O
 							return 0; 
 						} 
 				
